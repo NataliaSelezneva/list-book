af82de89b1d7d8f3f9be47bda96ef3dfdcc1a490
unknown <nstekhina@gmail.com>
2023-01-18 08:49:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
+
+  const[readBook, setReadBook] = useState(data);
+
+  const removeBook = (id) =>{
+    let deleteBook = readBook.filter(book => book.id !== id);
+    setReadBook(deleteBook);
+  }
+
+  const[annotation, setAnnotation] = useState(0);
+  const{book, annotationBook} = data[annotation];
+
+  const[showAllText, setShowAllText] = useState(false);
+
+  const previousAnnotation = () =>{
+    setAnnotation(annotation =>{
+      annotation --;
+
+      if(annotation<0){
+        return data.length -1;
+      }
+      return annotation;
+    })
+  }
+
+  const nextAnnotation = () =>{
+    setAnnotation(annotation =>{
+      annotation ++;
+      if(annotation>data.length -1){
+        annotation = 0;
+      }
+      return annotation;
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      
+      <div className='header'>
+        <h3>Мій список книг для читання в 2023 році</h3>
+      </div>
+
+      <div>
+        {readBook.map((nameBook =>{
+          const{id, book, imageBook} = nameBook;
+
+        return(
+
+        <div key={id}>
+
+          <div>
+          <p className='nameBook'>{id}. {book}</p>
+          </div>
+        
+          <div>
+          <img className='styleImage' src={imageBook} alt="book" width='140px'/>
+          </div>
+        
+          <div>
+          <button className='styleButtonRemote' onClick={ () => removeBook(id)}>Remote</button>
+          </div>
+        </div> 
+      )
+        }))}
+
+        <div>
+        <button className='styleButtonDeleteAll' onClick={ () => setReadBook([])}>Delete all</button>
+        </div>
+
+        <div className='styleBlockAnnotation'>
+        <div>
+          <h3>Анотація до книг</h3>
+        </div>
+        <div>
+          <p>{book}</p>
+        </div>
+        <div>
+          <p className='styleText'>{showAllText ? annotationBook : annotationBook.substring(0, 100) + '...'}
+          <button className='styleButtonAnnotation' onClick={() =>setShowAllText(!showAllText)}>{showAllText ? 'Звернути' : 'Показати більше'}</button></p>
+        </div>
+        <div>
+          <button className='styleButtonNextAndPrevious' onClick={previousAnnotation}>Попередня</button>
+          <button className='styleButtonNextAndPrevious' onClick={nextAnnotation}>Слідуюча</button>
+        </div>
+      </div>
+      </div>      
     </div>
   );
 }
